e2e0075fdcb2d188748b677fbdc6f6f37a106735
Tran Tu Quang <tuquagtran123@gmail.com>
2023-11-21 00:15:20 +0700

    feat: add login endpoint controller & version 1.0.1

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+const config = {
+  baseURL: "https://thingsboard.cloud",
+};
+
+export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
-export function greet(name: string): string {
-  return `Hello ${name}`;
+import axios, { AxiosInstance } from "axios";
+import config from "./config";
+
+interface LoginEndpointBody {
+  username: string;
+  password: string;
+}
+
+interface LoginEndpointResponse {
+  token: string;
+  refreshToken: string;
+  expiredIn: number;
+}
+export default class ThingsboardApiClient {
+  private instance: AxiosInstance;
+
+  constructor(baseURL?: string) {
+    this.instance = axios.create({
+      baseURL: baseURL || config.baseURL,
+    });
+  }
+
+  public async loginEndpoint(body: LoginEndpointBody) {
+    try {
+      const url = `/api/auth/login`;
+      return await this.instance.post<LoginEndpointResponse>(url, body);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
